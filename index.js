8be70eee94095d4d91bd8bdb51e0a7f4ef3783f7
Lucy <Charles@lucys-mac.mynet>
2018-10-22 22:28:08 +0100

    new index and old index, working on functions

--- index.js
+++ index.js
@@ -1,104 +1,89 @@
-// import { getResult } from "./src/equalsClick";
+import { getResult } from "./src/equalsClick";
 
-// let displayNum = "";
-// let storeSum = "";
-// let storeNum1 = "";
-// let storeOperator = "";
-// let storeNum2 = "";
-// let decimal = "";
+let displayNum = "";
+let storeSum = [];
+let storeOperator = "";
+let storeNum = "";
+let decimal = "";
 
-// let display = document.getElementById("display");
-// let answer = document.getElementById("answer");
-// let rt = document.getElementById("running");
+let display = document.getElementById("display");
+let rt = document.getElementById("running");
+console.log("test");
+const clearDisplay = arg => {
+  displayNum = "";
+  storeSum = [];
+  storeNum = "";
+  storeOperator = "";
+  decimal = "";
+  display.innerHTML = displayNum;
+  rt.innerHTML = storeSum;
+};
+let clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearDisplay);
 
-// const clearDisplay = arg => {
-//   displayNum = "";
-//   storeSum = "";
-//   storeNum1 = "";
-//   storeNum2 = "";
-//   decimal = "";
-//   storeOperator = "";
-//   display.innerHTML = displayNum;
-//   rt.innerHTML = storeSum;
-//   answer.innerHTML = "";
-// };
-// let clearButton = document.getElementById("clear");
-// clearButton.addEventListener("click", clearDisplay);
+const numClick = e => {
+  storeNum += e.target.value;
+  storeSum += e.target.value;
+  displayNum += e.target.value;
+  display.innerHTML = displayNum;
+  rt.innerHTML = storeSum;
+};
+let numButtons = document.getElementsByClassName("number");
+for (let i = 0; i < numButtons.length; i++) {
+  console.log(numButtons[i]);
+  numButtons[i].addEventListener("click", numClick);
+}
 
-// const numClick = e => {
-//   displayNum += e.target.value;
-//   storeSum += e.target.value;
-//   if (e.target.value === 0) {
+const pointOperator = (arg, e) => {
+  if (displayNum === "") {
+    arg += 0;
+    arg += e.target.value;
+    decimal += e.target.value;
+    displayNum += 0;
+    displayNum += decimal;
+    storeSum += 0;
+    storeSum += e.target.value;
+  } else if (decimal === "") {
+    arg += e.target.value;
+    decimal += e.target.value;
+    displayNum += decimal;
+    storeSum += decimal;
+  } else if (decimal.length > 0) {
+    return;
+  }
+};
 
-//   }
-//   if (storeOperator === "") {
-//     storeNum1 += e.target.value;
-//   } else if (storeOperator) {
-//     storeNum2 += e.target.value;
-//   }
-//   display.innerHTML = displayNum;
-//   rt.innerHTML = storeSum;
-//   answer.innerHTML = "";
-// };
-// let numButtons = document.getElementsByClassName("number");
-// for (let i = 0; i < numButtons.length; i++) {
-//   console.log(numButtons[i]);
-//   numButtons[i].addEventListener("click", numClick);
-// }
+const decimalClick = e => {
+  pointOperator(storeNum, e);
+  display.innerHTML = displayNum;
+  rt.innerHTML = storeSum;
+};
+let pointButton = document.getElementById("point");
+pointButton.addEventListener("click", decimalClick);
 
-// const pointOperator = (arg, e) => {
-//   if (displayNum === "") {
-//     arg += 0;
-//     arg += e.target.value;
-//     decimal += e.target.value;
-//     displayNum += 0;
-//     displayNum += decimal;
-//     storeSum += 0;
-//     storeSum += e.target.value;
-//   } else if (decimal === "") {
-//     arg += e.target.value;
-//     decimal += e.target.value;
-//     displayNum += decimal;
-//     storeSum += decimal;
-//   } else if (decimal.length > 0) {
-//     return;
-//   }
-// };
+const operatorClick = e => {
+  displayNum = "";
+  decimal = "";
+  storeSum += e.target.value;
+  storeOperator += e.target.value;
+  display.innerHTML = displayNum;
+};
+let operatorButtons = document.getElementsByClassName("action");
+for (let i = 0; i < operatorButtons.length; i++) {
+  console.log(operatorButtons[i]);
+  operatorButtons[i].addEventListener("click", operatorClick);
+}
 
-// const decimalClick = e => {
-//   if (storeOperator === "") {
-//     pointOperator(storeNum1, e);
-//   } else if (storeOperator) {
-//     pointOperator(storeNum2, e);
-//   }
-//   display.innerHTML = displayNum;
-//   answer.innerHTML = "";
-//   rt.innerHTML = storeSum;
-// };
-// let pointButton = document.getElementById("point");
-// pointButton.addEventListener("click", decimalClick);
+const equalsClick = arg => {
+  let sumArr = sum.split("+", "-", "*", "/");
+  console.log(sumArr);
+  display.innerHTML = result;
+  displayNum = "";
+  storeOperator = "";
+  storeSum = "";
+};
 
-// const operatorClick = e => {
-//   displayNum = "";
-//   decimal = "";
-//   storeSum += e.target.value;
-//   storeOperator += e.target.value;
-//   display.innerHTML = displayNum;
-// };
-// let operatorButtons = document.getElementsByClassName("action");
-// for (let i = 0; i < operatorButtons.length; i++) {
-//   console.log(operatorButtons[i]);
-//   operatorButtons[i].addEventListener("click", operatorClick);
-// }
+let equalsButton = document.getElementById("equals");
+equalsButton.addEventListener("click", equalsClick);
 
-// const equalsClick = arg => {
-//   const result = getResult(storeNum1, storeOperator, storeNum2);
-//   answer.innerHTML = result;
-//   displayNum = "";
-//   storeNum1 = "";
-//   storeOperator = "";
-//   storeNum2 = "";
-// };
-
-// let equalsButton = document.getElementById("equals");
-// equalsButton.addEventListener("click", equalsClick);
+console.log(displayNum, storeSum, storeOperator, decimal);
