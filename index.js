d6f67b5b1573b966e4d5dd47b41fb7357bfcd8a1
Lucy <Charles@lucys-mac.mynet>
2018-10-23 11:31:36 +0100

    limited length of input number

--- index.js
+++ index.js
@@ -22,35 +22,38 @@ let clearButton = document.getElementById("clear");
 clearButton.addEventListener("click", clearDisplay);
 
 const numClick = e => {
-  storeNum += e.target.value;
-  storeSum += e.target.value;
-  displayNum += e.target.value;
+  if (storeNum.length < 15) {
+    storeNum += e.target.value;
+    storeSum += e.target.value;
+    displayNum += e.target.value;
+  }
   display.innerHTML = displayNum;
   rt.innerHTML = storeSum;
 };
 let numButtons = document.getElementsByClassName("number");
 for (let i = 0; i < numButtons.length; i++) {
-  console.log(numButtons[i]);
   numButtons[i].addEventListener("click", numClick);
 }
 
 // TODO: add conditions for 0
 
 const pointOperator = (arg, e) => {
-  if (displayNum === "") {
-    arg += 0;
-    arg += e.target.value;
-    decimal += e.target.value;
-    displayNum += 0;
-    displayNum += decimal;
-    storeSum += 0;
-    storeSum += e.target.value;
-  } else if (decimal === "") {
-    arg += e.target.value;
-    decimal += e.target.value;
-    displayNum += decimal;
-    storeSum += decimal;
-  } else if (decimal.length > 0) {
+  if (storeNum < 14) {
+    if (displayNum === "") {
+      arg += 0;
+      arg += e.target.value;
+      decimal += e.target.value;
+      displayNum += 0;
+      displayNum += decimal;
+      storeSum += 0;
+      storeSum += e.target.value;
+    } else if (decimal === "") {
+      arg += e.target.value;
+      decimal += e.target.value;
+      displayNum += decimal;
+      storeSum += decimal;
+    } else if (decimal.length > 0) {
+    }
   }
 };
 
@@ -65,13 +68,14 @@ pointButton.addEventListener("click", decimalClick);
 const operatorClick = e => {
   displayNum = "";
   decimal = "";
+  storeNum = "";
   storeSum += e.target.value;
   storeOperator += e.target.value;
   display.innerHTML = displayNum;
+  rt.innerHTML = storeSum;
 };
 let operatorButtons = document.getElementsByClassName("action");
 for (let i = 0; i < operatorButtons.length; i++) {
-  console.log(operatorButtons[i]);
   operatorButtons[i].addEventListener("click", operatorClick);
 }
 
@@ -82,7 +86,12 @@ const equalsClick = sum => {
   storeSum = "";
 };
 
+// TODO: Use toFixed() to limit number of decimal places
+// let fixed = (0.1 + 0.2).toFixed(6);
+// console.log(fixed);
+// let reduce = Number(fixed).toPrecision(2);
+// console.log(reduce);
+// TODO: work out how many 0's at the end, reduce to length - that many.
+
 let equalsButton = document.getElementById("equals");
 equalsButton.addEventListener("click", equalsClick);
-
-console.log(displayNum, storeSum, storeOperator, decimal);
